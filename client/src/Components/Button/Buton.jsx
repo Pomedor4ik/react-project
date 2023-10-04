8757361eb0e28d9b9fab12047e1c0e0b62cd8968
Pomedor4ik <fpomedor4ik@gmail.com>
2023-10-04 20:31:55 +0300

    done

---
+++ client/src/Components/Button/Buton.jsx
@@ -0,0 +1,10 @@
+const Button = (props) => {
+    return(
+        // <button>Sign in</button>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
